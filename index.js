a4956dd18e6f47dc7a234ee6140486c500a2bd12
joeyjira <joey.jira@gmail.com>
2017-09-13 12:11:55 -0700

    positioned tray

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const path = require('path');
-const electron = require('electron');
+const path = require("path");
+const electron = require("electron");
 
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        heigth: 500,
-        width: 300,
-        frame: false,
-        resizable: false
-    });
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    heigth: 500,
+    width: 300,
+    frame: false,
+    resizable: false,
+    show: false
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
-    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
-    const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
-    new Tray(iconPath);
+  const iconName =
+    process.platform === "win32" ? "windows-icon.png" : "iconTemplate.png";
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  tray = new Tray(iconPath);
+  tray.on("click", (event, bounds) => {
+    const { x, y } = bounds;
+    const { height, width } = mainWindow.getBounds();
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: y,
+        heigth: height,
+        width: width
+      });
+      mainWindow.show();
+    }
+  });
 });
